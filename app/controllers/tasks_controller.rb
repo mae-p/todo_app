f07fe971108232b99049c07746c9078f31ba3abd
mae-p <yasutake.m@aisaac.jp>
2023-05-07 15:20:12 +0900

    First commit

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,18 @@
+class TasksController < ApplicationController
+    def index
+        @tasks = Task.all
+    end
+
+    def new
+        @task = Task.new
+    end
+
+    def create
+    end
+
+    def dene
+    end
+
+    def destroy
+    end
+end
