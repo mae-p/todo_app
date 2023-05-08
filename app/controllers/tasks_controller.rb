c4a0689c3fdf4ba28318bce26e8b52227ffb9dd1
mae-p <yasutake.m@aisaac.jp>
2023-05-08 23:32:36 +0900

    alert

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,24 +1,30 @@
 class TasksController < ApplicationController
   def index
     @task = Task.new
-    @tasks = Task.where(done_at:nil)
-    @tasks_done = Task.where.not(done_at:nil).order(:time)
+    @tasks = Task.where(done_at:nil).order(:deadline)
+    @tasks_done = Task.where.not(done_at:nil)
+    @today = Date.today
   end
 
   def create
-    task = Task.new(task_params)
-    task.save
-    redirect_to root_path
+    @task = Task.new(task_params)
+    if @task.save
+      flash[:success] = '登録が成功しました'
+      redirect_to root_path
+    else
+      flash[:danger] = '項目を入力してください'
+    end
   end
 
   def done
     @today = Date.today
-    task = Task.find(params[:id])
-    task.update(done_at: @today)
+    @task = Task.find(params[:id])
+    @task.update(done_at: @today)
     redirect_to root_path
   end
 
-  private def task_params
-    params.require(:task).permit(:title, :deadline)
-   end
+  private 
+    def task_params
+      params.require(:task).permit(:title, :deadline)
+    end
 end
