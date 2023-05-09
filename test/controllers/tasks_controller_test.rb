9115a16e0b99267fd6acd30430fffe007dc0d731
mae-p <yasutake.m@aisaac.jp>
2023-05-10 01:31:19 +0900

    improve

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -1,8 +1,7 @@
 require "test_helper"
 
 class TasksControllerTest < ActionDispatch::IntegrationTest
-  test "should get root" do
-    get root_path
-    assert_response :success
-  end
+  # test "the truth" do
+  #   assert true
+  # end
 end
