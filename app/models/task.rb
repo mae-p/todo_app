b95a1bc7996dd2e9627a6cc3c7c7efcd73a7c565
mae-p <yasutake.m@aisaac.jp>
2023-05-11 15:33:28 +0900

    add valiate

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,11 @@
 class Task < ApplicationRecord
   validates :title,  presence: true
   validates :deadline, presence: true
+  validate :deadline_before_today
+    
+    
+    def deadline_before_today
+      return if deadline.blank?
+      errors.add(:deadline, "今日以降のものを選択する") if deadline < Date.today
+    end
 end
