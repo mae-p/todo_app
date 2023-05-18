f10a13f6d2c5fcb7da807b8d25ddca18cf9bcfe4
mae-p <yasutake.m@aisaac.jp>
2023-05-18 20:26:52 +0900

    add delete

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'tasks#index'
+  delete 'tasks/:id/destroy' => 'tasks#destroy', as: 'articles_destroy'
   resources :tasks do
     member do
       post :done
