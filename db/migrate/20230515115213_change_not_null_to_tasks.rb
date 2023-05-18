aab4e7519aa32f724ffab1565c9127229b17a390
mae-p <yasutake.m@aisaac.jp>
2023-05-18 15:13:44 +0900

    Add priority

--- db/migrate/20230515115213_change_not_null_to_tasks.rb
+++
@@ -1,5 +0,0 @@
-class ChangeNotNullToTasks < ActiveRecord::Migration[7.0]
-  def change
-    change_column :tasks, :priority, :integer, default: "", null: false
-  end
-end
--- db/migrate/20230518061050_add_priority_to_tasks.rb
+++ db/migrate/20230518061050_add_priority_to_tasks.rb
@@ -1,5 +1,5 @@
 class AddPriorityToTasks < ActiveRecord::Migration[7.0]
   def change
-    add_column :tasks, :priority, :integer
+    add_column :tasks, :priority, :integer :
   end
 end
